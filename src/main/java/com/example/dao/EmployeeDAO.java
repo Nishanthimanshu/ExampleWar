57b906aa847c8e4e73c30c198f4422f50dfaf000
Nishanthimanshu <66844313+Nishanthimanshu@users.noreply.github.com>
2022-09-26 12:05:18 +0530

    Update EmployeeDAO.java

--- src/main/java/com/example/dao/EmployeeDAO.java
+++ src/main/java/com/example/dao/EmployeeDAO.java
@@ -29,7 +29,7 @@ public class EmployeeDAO {
     	for(Employee e: list.getEmployeeList()) {
     		if(e.getId()==id) {
     			list.getEmployeeList().remove(e);
-    			returns true;
+    			return true;
     		}
     	}
     	return false;
